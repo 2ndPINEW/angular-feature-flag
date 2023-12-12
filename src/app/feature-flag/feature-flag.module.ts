8bc1d76314038364b4439b176db3e0c1000882e8
Eito Takase <eito01178@gmail.com>
2023-12-13 03:33:02 +0900

    create feature-flag.service

--- src/app/feature-flag/feature-flag.module.ts
+++ src/app/feature-flag/feature-flag.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FeatureFlagService } from './feature-flag.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [FeatureFlagService],
 })
-export class FeatureFlagModule { }
+export class FeatureFlagModule {}
---
+++ src/app/feature-flag/feature-flag.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+type Feature = 'feature1' | 'feature2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FeatureFlagService {
+  isFeatureEnabled(featureName: Feature): boolean {
+    switch (featureName) {
+      case 'feature1':
+        return true;
+      case 'feature2':
+        return false;
+      default:
+        return false;
+    }
+  }
+}
